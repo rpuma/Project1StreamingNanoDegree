b3f13e891b260a940082e1e08c5822acc6a9bb57
Ricardo <rpuma.alvarez@gmail.com>
2021-04-28 00:58:41 -0500

    fixed turnstile topic name

--- producers/models/turnstile.py
+++ producers/models/turnstile.py
@@ -36,7 +36,7 @@ class Turnstile(Producer):
         #
         super().__init__(
             # TODO: Come up with a better topic name
-            "org.chicago.cta.station.turnstile.{station_name}.v1",
+            f"org.chicago.cta.station.turnstile.{station_name}.v1",
             key_schema=Turnstile.key_schema,
             value_schema=Turnstile.value_schema,
             num_partitions=2,
