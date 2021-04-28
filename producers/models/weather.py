f74ed8f664c96d15a7bcd47e3ee67715b02f70d5
Ricardo <rpuma.alvarez@gmail.com>
2021-04-28 13:23:25 -0500

    Cast weather schemas as string

--- producers/models/weather.py
+++ producers/models/weather.py
@@ -90,8 +90,8 @@ class Weather(Producer):
             headers={"Content-Type": "application/vnd.kafka.avro.v2+json"},
             data=json.dumps(
                 {
-                   "value_schema": Weather.value_schema,
-                   "key_schema": Weather.key_schema,
+                   "value_schema": str(Weather.value_schema),
+                   "key_schema": str(Weather.key_schema),
                    "records":[{"key": self.time_millis(), "value":{"temperature":self.temp, "status": self.status.name}}]
                 }
             )
